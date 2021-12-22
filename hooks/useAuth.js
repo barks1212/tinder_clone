5ec02eaff9571c60aae04e548d7936c82c341b66
William Barker <willbarkerwork@gmail.com>
2021-12-22 14:05:08 +0000

    env working

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,12 +1,24 @@
 import React, { createContext, useContext } from "react";
+import * as Google from "expo-google-app-auth";
+import { IOS_CLIENT_ID } from "@env";
 
 const AuthContext = createContext({});
 
+const config = {
+  iosClientId: IOS_CLIENT_ID,
+  scopes: ["profile", "email"],
+  permissions: ["public_profile", "email", "gender", "location"],
+};
+
 export const AuthProvider = ({ children }) => {
+  const signInWithGoogle = async () => {
+    await Google.loginAsync();
+  };
+
   return (
     <AuthContext.Provider
       value={{
-        user: "Will",
+        user: null,
       }}
     >
       {children}
