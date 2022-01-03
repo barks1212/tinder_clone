24cb0aa0a2304b8cb18098fcc29c4b1a0be05dba
William Barker <willbarkerwork@gmail.com>
2022-01-03 16:33:34 +0000

    cards with dummy data

--- constants.js
+++ constants.js
@@ -1,3 +1,35 @@
 export const colours = {
   tinderPink: "#FF5864",
+  match: "#4DED30",
+  swiperBackground: "#4FD0E9",
 };
+
+export const dummyData = [
+  {
+    firstName: "Will",
+    lastName: "Barker",
+    job: "Software Developer",
+    photoURL:
+      "https://scontent-man2-1.xx.fbcdn.net/v/t1.6435-9/46503573_10161487665760352_7497734905527271424_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=FuciG_VL838AX9dnk-6&_nc_ht=scontent-man2-1.xx&oh=00_AT90LOn2KupRiqVcFDYiUIhqvNPBeV2V3ajLVZwUktVk-Q&oe=61F6EF88",
+    age: 30,
+    id: 123,
+  },
+  {
+    firstName: "Katie",
+    lastName: "Holliday",
+    job: "Photographer",
+    photoURL:
+      "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/241560358_10160156794279505_7635732381855750343_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0--3Gn6FGycAX-5iMjz&tn=_DIFWZVQcZdIEvLM&_nc_ht=scontent-man2-1.xx&oh=00_AT-LkNXhjjfnnNX-XRHscpn7rNxrDTGikAleXl8OvzKaMg&oe=61D8BB6A",
+    age: 29,
+    id: 456,
+  },
+  {
+    firstName: "Elon",
+    lastName: "Musk",
+    job: "Psychopath",
+    photoURL:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQU2JRbbl3LBOm_an3eI5iplFhOoLESyBwUfmWDO49BS1EYuGUE",
+    age: 23891,
+    id: 789,
+  },
+];
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useRef } from "react";
 import {
   View,
   SafeAreaView,
@@ -7,18 +7,22 @@ import {
   Button,
   TouchableOpacity,
   Image,
+  StyleSheet,
 } from "react-native";
 import tw from "tailwind-rn";
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
 import useAuth from "../hooks/useAuth";
-import { colours } from "../constants";
+import { colours, dummyData } from "../constants";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const { user, logout } = useAuth();
 
+  const swipeRef = useRef(null);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={tw("flex-1")}>
       <View style={tw("flex-row items-center justify-between px-5")}>
         <TouchableOpacity onPress={logout}>
           <Image
@@ -40,14 +44,104 @@ const HomeScreen = () => {
           />
         </TouchableOpacity>
       </View>
-      {/* <Text>Home screenss</Text>
-      <Button
-        title="Go to Chat Screen"
-        onPress={() => navigation.navigate("Chat")}
-      />
-      <Button title="Logout" onPress={logout} /> */}
+
+      <View style={tw("flex-1 -mt-6")}>
+        <Swiper
+          ref={swipeRef}
+          containerStyle={{ backgroundColor: "transparent" }}
+          cards={dummyData}
+          stackSize={5}
+          cardIndex={0}
+          verticalSwipe={false}
+          animateCardOpacity
+          onSwipedLeft={() => {
+            console.log("Swipe PASS");
+          }}
+          onSwipedRight={() => {
+            console.log("Swipe MATCH");
+          }}
+          backgroundColor={colours.swiperBackground}
+          overlayLabels={{
+            left: {
+              title: "NOPE",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                },
+              },
+            },
+            right: {
+              title: "MATCH",
+              style: {
+                label: {
+                  color: colours.match,
+                },
+              },
+            },
+          }}
+          renderCard={(card) => (
+            <View
+              key={card.id}
+              style={tw("relative bg-white h-3/4 rounded-xl")}
+            >
+              <Image
+                style={tw("absolute top-0 h-full w-full rounded-xl")}
+                source={{ uri: card.photoURL }}
+              />
+              <View
+                style={[
+                  tw(
+                    "absolute bottom-0 bg-white w-full flex-row justify-between items-center h-20 px-6 py-2 rounded-b-xl"
+                  ),
+                  styles.cardShadow,
+                ]}
+              >
+                <View>
+                  <Text style={tw("text-xl font-bold")}>
+                    {card.firstName} {card.lastName}
+                  </Text>
+                  <Text>{card.job}</Text>
+                </View>
+                <Text style={tw("text-2xl font-bold")}>{card.age}</Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+      <View style={tw("flex flex-row justify-evenly")}>
+        <TouchableOpacity
+          onPress={() => swipeRef.current.swipeLeft()}
+          style={tw(
+            "items-center justify-center rounded-full w-16 h-16 bg-red-200"
+          )}
+        >
+          <Entypo name="cross" size={35} color="red" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => swipeRef.current.swipeRight()}
+          style={tw(
+            "items-center justify-center rounded-full w-16 h-16 bg-green-200"
+          )}
+        >
+          <Entypo name="heart" size={35} color="green" />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+});
